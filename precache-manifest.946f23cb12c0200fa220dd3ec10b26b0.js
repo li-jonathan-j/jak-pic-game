594ff6a8ebf8e2b9ae4c5a36a6c236df2d30a3cb
Jia Li <li.jonathan.j@gmail.com>
2020-05-24 11:27:49 -0500

    Updates

---
+++ precache-manifest.946f23cb12c0200fa220dd3ec10b26b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13270e4ea971d87c8cc0e5c8900a54a3",
+    "url": "/jak-pic-game/index.html"
+  },
+  {
+    "revision": "6624b0a6041eb4bf803d",
+    "url": "/jak-pic-game/static/css/main.749e20b6.chunk.css"
+  },
+  {
+    "revision": "ded445355dddddec044c",
+    "url": "/jak-pic-game/static/js/2.80747501.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/jak-pic-game/static/js/2.80747501.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6624b0a6041eb4bf803d",
+    "url": "/jak-pic-game/static/js/main.b506511c.chunk.js"
+  },
+  {
+    "revision": "7d2bc2d28b698187d9da",
+    "url": "/jak-pic-game/static/js/runtime-main.31fd385d.js"
+  }
+]);
